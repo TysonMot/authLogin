b7a68e0505e4dca5c020098efaecac4da167fac6
tyson <tysonmotlhabeng@gmail.com>
2021-10-18 20:24:14 +0200

    about user

--- components/about.jsx
+++ components/about.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Button, FlatList, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View} from "react-native";
+import {Button, FlatList, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View} from "react-native";
 import Users from "./users"
 import {Avatar, Header, ListItem} from "react-native-elements";
 import firebase from "./firebase";
@@ -9,13 +9,14 @@ const About = ( navigation ) => {
     
     const [name,setName ] = useState('')
     const [email,setEmail ] = useState()
- 
-    
+    const  img  = 'https://www.seekpng.com/png/detail/428-4287240_no-avatar-user-circle-icon-png.png'
+
     useEffect(() => {
         const id = localStorage.getItem('userid')
         Users.getLoggedData(id).on('value', snapshot => {
             const data = snapshot.val()
             setName(data.name)
+            setEmail(data.email)
         })
     })
     
@@ -32,6 +33,22 @@ const About = ( navigation ) => {
               </Text>
           </Pressable>
        </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.tinyLogo} source={{ uri : 'https://static.thenounproject.com/png/2518416-200.png'}}/>
+            </View>
+            <View style={styles.heading}>
+                <Text style={styles.heading}>
+                    User data
+                </Text>
+            </View>
+            <View style={styles.userContainer}> 
+                <Text>
+                     { name }
+                </Text>
+                <Text>
+                     { email }
+                </Text>
+            </View>
         </>
     )
 }
@@ -46,6 +63,16 @@ const styles = new StyleSheet.create({
         borderBottomRightRadius: 50,
         textAlign: "center"
     },
+    imageContainer :{
+        width: '100%',
+        padding:40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tinyLogo: {
+        width: 100,
+        height: 100,
+    },
     logout : {
         color: 'white',
         paddingTop:23
@@ -54,6 +81,16 @@ const styles = new StyleSheet.create({
         textTransform: "uppercase",
         color: 'white',
         fontWeight: "900"
+    },
+    heading: {
+        textAlign:"center",
+        fontSize: 20,
+        textTransform: "uppercase",
+        fontWeight: "900"
+    },
+    userContainer : {
+        textAlign: 'center',
+        marginTop: 20
     }
 })
 
